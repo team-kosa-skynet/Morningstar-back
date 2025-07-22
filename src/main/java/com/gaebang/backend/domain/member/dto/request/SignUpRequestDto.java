226614941036c8ec29c00de5acacbc4f96d35987
liyusang1 <liyusang1@naver.com>
2025-07-22 15:45:21 +0900

    feat : 회원가입시 닉네임 자동 생성 로직 적용

--- src/main/java/com/gaebang/backend/domain/member/dto/request/SignUpRequestDto.java
+++ src/main/java/com/gaebang/backend/domain/member/dto/request/SignUpRequestDto.java
@@ -1,32 +1,24 @@
 package com.gaebang.backend.domain.member.dto.request;
 
 import com.gaebang.backend.domain.member.entity.Member;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public record SignUpRequestDto(
 
-    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "이메일 형식이 유효하지 않습니다")
-    @NotNull(message = "email은 필수값입니다")
-    String email,
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "이메일 형식이 유효하지 않습니다")
+        @NotNull(message = "email은 필수값입니다")
+        String email,
 
-    @NotNull(message = "password는 필수값입니다")
-    String password,
-
-    @NotBlank(message = "닉네임은 필수값입니다.")
-    @Size(max = 10, message = "닉네임은 최대 10자까지 입력 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 한글, 영문, 숫자만 사용 가능합니다.")
-    String nickname
+        @NotNull(message = "password는 필수값입니다")
+        String password
 ) {
-
-    public Member toEntity(String encodedPassword) {
+    public Member toEntity(String encodedPassword, String generatedNickname) {
         return Member.builder()
-            .email(email)
-            .nickname(nickname)
-            .password(encodedPassword)
-            .authority("ROLE_USER")
-            .build();
+                .email(email)
+                .nickname(generatedNickname)
+                .password(encodedPassword)
+                .authority("ROLE_USER")
+                .build();
     }
 }
