c7111c03c34c55416d7dac1e71922034ca208fd5
minimini <zjonyv12@naver.com>
2025-07-23 09:58:10 +0900

    feat: 포인트 시스템 구현 - 프로토타입 완성

--- src/main/java/com/gaebang/backend/domain/point/dto/response/PointResponseDto.java
+++ src/main/java/com/gaebang/backend/domain/point/dto/response/PointResponseDto.java
@@ -2,6 +2,8 @@ package com.gaebang.backend.domain.point.dto.response;
 
 import com.gaebang.backend.domain.point.entity.Point;
 import com.gaebang.backend.domain.point.entity.PointType;
+import com.gaebang.backend.global.util.DataFormatter;
+
 import java.time.LocalDateTime;
 
 // 전체 포인트 내역을 조회하기 위한 responseDto
@@ -12,7 +14,8 @@ public record PointResponseDto (
         PointType type,
         Integer depositSum,
         Integer withdrawSum,
-        LocalDateTime date
+//        LocalDateTime data 로 했을 경우 response 가 [ ... ] 이렇게 배열형식으로 넘어온다
+        String date
 ) {
 
     public static PointResponseDto fromEntity(Point point) {
@@ -23,7 +26,11 @@ public record PointResponseDto (
                 point.getType(),
                 point.getDepositSum(),
                 point.getWithdrawSum(),
-                point.getDate()
+//              point.getDate()
+
+                // 이렇게 DataFormatter를 사용하면 "date": "2025.07.23" 이러한 형식으로 넘어온다
+                // 위의 출력값은 DataFormatter.getFormattedCreatedAt() 으로 설정했을 때
+                DataFormatter.getFormattedCreatedAtWithTime(point.getDate())
         );
     }
 
