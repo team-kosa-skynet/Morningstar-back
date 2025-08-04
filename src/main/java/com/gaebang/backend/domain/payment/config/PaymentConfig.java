1c27516d67d2e0e62b4ab935f12621087a506eb0
minimini <zjonyv12@naver.com>
2025-08-04 14:35:38 +0900

    fix: restClient 추가

--- src/main/java/com/gaebang/backend/domain/payment/config/PaymentConfig.java
+++ src/main/java/com/gaebang/backend/domain/payment/config/PaymentConfig.java
@@ -2,14 +2,18 @@ package com.gaebang.backend.domain.payment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class PaymentConfig {
-
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplate() {  // Payment 도메인에서 사용
         return new RestTemplate();
     }
 
+    @Bean
+    public RestClient restClient() {       // Question 도메인에서 사용
+        return RestClient.builder().build();
+    }
 }
