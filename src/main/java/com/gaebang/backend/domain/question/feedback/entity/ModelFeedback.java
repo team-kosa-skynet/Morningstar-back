0f6a975081f3dfc3dcb7d1604511b29c5ee1414c
minimini <zjonyv12@naver.com>
2025-08-26 11:26:14 +0900

    fix: dto, entity 수정 => 필수, null 허용 구분

--- src/main/java/com/gaebang/backend/domain/question/feedback/entity/ModelFeedback.java
+++ src/main/java/com/gaebang/backend/domain/question/feedback/entity/ModelFeedback.java
@@ -23,21 +23,21 @@ public class ModelFeedback extends BaseTimeEntity {
     private Member member;
 
     // 긍정적으로 평가한 모델명
-    @Column(name = "positive_model")
+    @Column(name = "positive_model", nullable = false)
     private String positiveModel;
 
     // 부정적으로 평가한 모델명
-    @Column(name = "negative_model")
+    @Column(name = "negative_model", nullable = false)
     private String negativeModel;
 
     // 긍정적 피드백 카테고리
     @Enumerated(EnumType.STRING)
-    @Column(name = "positive_feedback")
+    @Column(name = "positive_feedback", nullable = false)
     private FeedbackCategory positiveFeedback;
 
     // 부정적 피드백 카테고리
     @Enumerated(EnumType.STRING)
-    @Column(name = "negative_feedback")
+    @Column(name = "negative_feedback", nullable = false)
     private FeedbackCategory negativeFeedback;
 
     @Column(name = "detailed_comment", length = 1000)
